913200858e81eed9d856fe3f89b61715683405a2
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-07-11 18:58:41 +0300

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from "lodash.throttle";
+const STORAGE_KEY = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
+const form = document.querySelector('.feedback-form')
+form.addEventListener('submit', onForm)
+form.addEventListener('input', throttle(onInput), 500)
+
+populateTextarea()
+
+function onForm(evt) {
+   evt.preventDefault();
+  localStorage.removeItem(STORAGE_KEY);
+    evt.currentTarget.reset();
+    const { email, message } = evt.currentTarget.elements;
+console.log({ email: email.value, message: message.value });
+}
+
+
+function onInput(evt) {   
+formData[evt.target.name] = evt.target.value.trim();
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+
+function populateTextarea() {
+if (formData) {
+ let { email, message } = form.elements;
+email.value = formData.email || "";
+message.value = formData.message || "";
+   }
+}
