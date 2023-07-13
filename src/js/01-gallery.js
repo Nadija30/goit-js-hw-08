c69dc61d84c0d2498f0e650eb75313c0cfb673cd
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-07-13 15:45:49 +0300

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,11 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
-import { closeModal } from '../helpers/closeModal';
-// Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-import * as basicLightbox from 'basiclightbox';
-import "basiclightbox/dist/basicLightbox.min.css";
 
-let instance;
-//console.log(galleryItems);
 const container = document.querySelector('.gallery');
 const markup = galleryItems.map(({ preview, original, description }) => 
-`<li class = "gallery__item">
+`
   <a class = "gallery__link" href = "${original}">
     <img
       class = "gallery__image"
@@ -20,40 +14,9 @@ const markup = galleryItems.map(({ preview, original, description }) =>
       alt = "${description}"
     />
   </a>
-</li >`);
-container.insertAdjacentHTML('beforeend', markup.join(''));
-container.addEventListener('click', onClick)
+  `).join('');
+container.insertAdjacentHTML('beforeend', markup);
 
-function onClick(evt) {
-     evt.preventDefault();
-if (evt.target.nodeName !== "IMG") {
-    return;  
-}
- //console.log(evt.target)
-    const imgSource = evt.target.dataset.source ?? evt.target.closest('img').dataset.source;
-  console.log(imgSource);
-
-  instance = basicLightbox.create(`
-    <img
-      src = "${imgSource}" "width="1280" height="auto"
-    />
-`, {
-      handler: null,
-      onShow(instance) {
-          this.handler = closeModal.bind(instance)
-        document.addEventListener('keydown', this.handler);
-      
-    },
-	
-    onClose() {
-    document.removeEventListener('keydown', this.handler);
-    }
-  }
-  );
-    instance.show();
-
-}
-
-const lightbox = new SimpleLightbox('.gallery a', {
+  const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
-});
+  });
